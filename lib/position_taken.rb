591649d1d642809c355a8bfac971e0cbff4c1816
Myra Rebillard <myra.rebillard@gmail.com>
2017-11-12 03:27:22 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(board, position)
-  
-  if(board[position - 1] == " " || board[position - 1] == "")
+
+  if(board[position - 1] == " ") || (board[position - 1] == "")
     return false
   end
 
-  return true    
+  return true
 end
