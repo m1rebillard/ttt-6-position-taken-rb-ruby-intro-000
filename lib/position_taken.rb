427b4f3457b4253514d22df4426576db4b502b1d
Myra Rebillard <myra.rebillard@gmail.com>
2017-11-12 03:36:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, position)
   if (board[position] == "X") || (board[position] == "O")
     return true
   end
-  
+
   if (board[position] == " ") || (board[position] == "") || (board[position] == nil)
     return false
   end
